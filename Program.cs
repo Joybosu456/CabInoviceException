d9d1a2857ebcda3e38f0c2c69bfd53aa88ca3687
JOY <joybosu@gmail.com>
2022-03-19 23:49:32 +0530

    welcome to my Cab Program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CabInoviceGenarator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my InvoiceGenerator Program");
+            InvoiceGenerator inoviceGenarator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = inoviceGenarator.CalculateFare(2.0, 5);
+            Console.WriteLine(fare);
+        }
+    }
+}
